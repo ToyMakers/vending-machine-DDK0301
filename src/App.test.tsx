7b3e7aafa949f0f77543e02aedee71cf20943ce9
jeongnaehyeok <cnh0301@gmail.com>
2021-01-03 15:46:32 +0900

    [ Setup ] 프로젝트 시작

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders test', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/test/i);
+  expect(linkElement).toBeInTheDocument();
+});
