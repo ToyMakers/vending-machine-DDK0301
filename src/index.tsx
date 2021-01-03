4cb68f85c1091b6c9c3bf87376ab8e9c61719638
jeongnaehyeok <cnh0301@gmail.com>
2021-01-04 00:00:21 +0900

    [ Setup ] Redux 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from '@/App';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
 
 import '@scss/project.scss';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
