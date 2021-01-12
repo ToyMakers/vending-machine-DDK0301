2486254715fef28e314dc8ea56053136b704d382
jeongnaehyeok <cnh0301@gmail.com>
2021-01-13 00:22:25 +0900

    [ Feature ] Cooler 추가

---
+++ src/components/commons/Cooler.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '@scss/commons/Cooler.scss';
+
+export default function Cooler() {
+  return (
+    <div className="cooler">
+      <div className="cooler__row">
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+      </div>
+      <div className="cooler__row">
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+        <div className="hole" />
+      </div>
+    </div>
+  );
+}
