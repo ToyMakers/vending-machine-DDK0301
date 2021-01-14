c6030b860ac19ba171c8ce3a10b349979ca226c8
jeongnaehyeok <cnh0301@gmail.com>
2021-01-14 22:47:21 +0900

    [ Feature ] App 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import MachineContainer from '@containers/MachineContainer';
+import UserStateContainer from '@containers/UserStateContainer';
 import '@scss/App.scss';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <div className="App__wrapper">
+        <MachineContainer />
+        <UserStateContainer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
